29ac15649b2e9c7dd591c9ac1dbb2a0d855abac3
Mickaël CHAVE <mchave3@live.fr>
2025-04-03 18:34:35 +0200

    Refactor Start-Bucket function to include ShouldProcess support for logging initialization

--- src/Bucket/Public/Start-Bucket.ps1
+++ src/Bucket/Public/Start-Bucket.ps1
@@ -22,21 +22,22 @@
 function Start-Bucket {
 
     #Requires -Version 5.1
-    #Requires -RunAsAdministrator
     #Requires -Modules PoShLog
 
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess)]
     param(
 
     )
 
     process {
-        New-Logger |
-        Set-MinimumLevel -Value Verbose | # You can change this value later to filter log messages
-        # Here you can add as many sinks as you want - see https://github.com/PoShLog/PoShLog/wiki/Sinks for all available sinks
-        Add-SinkConsole |   # Tell logger to write log messages to console
-        Add-SinkFile -Path 'C:\Data\my_awesome.log' | # Tell logger to write log messages into file
-        Start-Logger
+        if ($PSCmdlet.ShouldProcess("Starting logger and initializing log files")) {
+            New-Logger |
+                Set-MinimumLevel -Value Verbose | # You can change this value later to filter log messages
+                # Here you can add as many sinks as you want - see https://github.com/PoShLog/PoShLog/wiki/Sinks for all available sinks
+                Add-SinkConsole |   # Tell logger to write log messages to console
+                Add-SinkFile -Path 'C:\Data\my_awesome.log' | # Tell logger to write log messages into file
+                Start-Logger # Start the logger
+        }
 
         # Get the current version of the Bucket module
         Get-BucketVersion
