ffd40413db9cafa3ce549680749bcd6ec498dc1b
Mickaël CHAVE <mchave3@live.fr>
2025-04-04 20:13:17 +0200

    Add GUI XAML file and integrate it into Start-Bucket function

--- src/Bucket/Public/Start-Bucket.ps1
+++ src/Bucket/Public/Start-Bucket.ps1
@@ -41,5 +41,37 @@ function Start-Bucket {
 
         # Get the current version of the Bucket module
         Get-BucketVersion
+
+        #region XAML
+        # Load the XAML file for the GUI
+        $inputXaml = Get-Content -Path "$PSScriptRoot\GUI\BucketGUI.xaml" -Raw
+
+        $inputXaml = $inputXaml -replace 'mc:Ignorable="d"', '' -replace 'x:N', 'N' -replace '^<Win.*', '<Window' -replace 'BucketVer', $script:BucketVersion
+        [void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
+        [void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+        [xml]$xaml = $inputXaml
+
+        $reader = (New-Object System.Xml.XmlNodeReader $xaml)
+        try {
+        $form = [Windows.Markup.XamlReader]::Load($reader)
+        } catch {
+        Write-Warning @"
+Unable to parse XML, with error: $($Error[0])
+Ensure that there are NO SelectionChanged or TextChanged properties in your textboxes
+(PowerShell cannot process them)
+"@
+            throw
+        }
+
+        # Load the XAML objects into variables
+        $xaml.SelectNodes('//*[@Name]') | ForEach-Object { "trying item $($_.Name)" | Out-Null
+            try { Set-Variable -Name "WPF$($_.Name)" -Value $form.FindName($_.Name) -ErrorAction Stop }
+            catch { throw }
+        }
+        #endregion XAML
+
+        #region Start GUI
+        $form.ShowDialog() | Out-Null
+        #endregion Start GUI
     }
 }
