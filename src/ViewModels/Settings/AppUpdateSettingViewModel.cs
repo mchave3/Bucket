9d6904fdf88069ddbd802671cbccbad4236c68c9
Mickaël CHAVE <mchave3@live.fr>
2025-06-18 16:43:47 +0200

    25.6.18.1 - Update update check logic and repository details in AppUpdateSettingViewModel

--- src/ViewModels/Settings/AppUpdateSettingViewModel.cs
+++ src/ViewModels/Settings/AppUpdateSettingViewModel.cs
@@ -41,14 +41,14 @@ namespace Bucket.ViewModels
             IsUpdateAvailable = false;
             IsCheckButtonEnabled = false;
             LoadingStatus = "Checking for new version";
+            Logger.Information("Update check status: {Status}", LoadingStatus);
             if (NetworkHelper.IsNetworkAvailable())
             {
                 Logger.Debug("Network is available, proceeding with update check");
                 try
                 {
-                    //Todo: Fix UserName and Repo
-                    string username = "";
-                    string repo = "";
+                    string username = "mchave3";
+                    string repo = "Bucket";
                     LastUpdateCheck = DateTime.Now.ToShortDateString();
                     Settings.LastUpdateCheck = DateTime.Now.ToShortDateString();
                     Logger.Debug("Checking for updates from repository: {Username}/{Repo}", username, repo);
@@ -58,22 +58,25 @@ namespace Bucket.ViewModels
                         IsUpdateAvailable = true;
                         ChangeLog = update.StableRelease.Changelog;
                         LoadingStatus = $"We found a new version {update.StableRelease.TagName} Created at {update.StableRelease.CreatedAt} and Published at {update.StableRelease.PublishedAt}";
+                        Logger.Information("New stable version found: {Version}, Status: {Status}", update.StableRelease.TagName, LoadingStatus);
                     }
                     else if (update.PreRelease.IsExistNewVersion)
                     {
                         IsUpdateAvailable = true;
                         ChangeLog = update.PreRelease.Changelog;
                         LoadingStatus = $"We found a new PreRelease Version {update.PreRelease.TagName} Created at {update.PreRelease.CreatedAt} and Published at {update.PreRelease.PublishedAt}";
+                        Logger.Information("New pre-release version found: {Version}, Status: {Status}", update.PreRelease.TagName, LoadingStatus);
                     }
                     else
                     {
                         LoadingStatus = "You are using latest version";
+                        Logger.Information("No updates available, Status: {Status}", LoadingStatus);
                     }
                 }
                 catch (Exception ex)
                 {
                     LoadingStatus = ex.Message;
-                    Logger.Error(ex, "Error occurred while checking for updates");
+                    Logger.Error(ex, "Error occurred while checking for updates, Status: {Status}", LoadingStatus);
                     IsLoading = false;
                     IsCheckButtonEnabled = true;
                 }
@@ -81,7 +84,7 @@ namespace Bucket.ViewModels
             else
             {
                 LoadingStatus = "Error Connection";
-                Logger.Warning("Network is not available, cannot check for updates");
+                Logger.Warning("Network is not available, cannot check for updates, Status: {Status}", LoadingStatus);
             }
             IsLoading = false;
             IsCheckButtonEnabled = true;
@@ -90,8 +93,7 @@ namespace Bucket.ViewModels
         [RelayCommand]
         private async Task GoToUpdateAsync()
         {
-            //Todo: Change Uri
-            await Launcher.LaunchUriAsync(new Uri("https://github.com/Ghost1372/DevWinUI/releases"));
+            await Launcher.LaunchUriAsync(new Uri("https://github.com/mchave3/Bucket/releases"));
         }
 
         [RelayCommand]
