6d66d9d5a7681693f93f9649c09301971cba6d73
Mickaël CHAVE <mchave3@live.fr>
2025-09-08 19:11:13 +0200

    feat: Add runtime property initialization for update channel and architecture in AppConfig

--- src/Bucket.App/Common/AppConfig.cs
+++ src/Bucket.App/Common/AppConfig.cs
@@ -1,5 +1,6 @@
 ﻿using Nucs.JsonSettings.Examples;
 using Nucs.JsonSettings.Modulation;
+using System.Runtime.InteropServices;
 
 namespace Bucket.App.Common
 {
@@ -24,6 +25,27 @@ namespace Bucket.App.Common
 
         // Whether this is the first time the application has been started
         private bool hasBeenStartedBefore { get; set; } = false;
+
+        // Update channel of the application (Release/Nightly)
+        private string updateChannel { get; set; } = "Release";
+
+        // Architecture of the application (x86/x64/arm64)
+        private string architecture { get; set; } = "x64";
+
+        public void InitializeRuntimeProperties()
+        {
+            // Automatically detect the update channel based on version
+            updateChannel = ProcessInfoHelper.Version.Contains("-Nightly") ? "Nightly" : "Release";
+            
+            // Automatically detect the system architecture
+            architecture = RuntimeInformation.ProcessArchitecture switch
+            {
+                System.Runtime.InteropServices.Architecture.X86 => "x86",
+                System.Runtime.InteropServices.Architecture.X64 => "x64", 
+                System.Runtime.InteropServices.Architecture.Arm64 => "arm64",
+                _ => "x64" // Default value
+            };
+        }
 
         // Docs: https://github.com/Nucs/JsonSettings
     }
